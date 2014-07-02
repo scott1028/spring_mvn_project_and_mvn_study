54dc35abe996e32f05122640d42316356d1270fd
scott.lan <scott.lan@taisys.com>
2014-07-02 17:35:15 +0800

    add json response

--- helloworld_sample/src/main/java/hello/HelloController.java
+++ helloworld_sample/src/main/java/hello/HelloController.java
@@ -2,13 +2,19 @@ package hello;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class HelloController {
-
+    // text/plain
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot by Scott!";
     }
 
+    // application/json 自動轉型態的
+    @RequestMapping("/json")
+    public String[] index_json(HttpServletResponse response){
+        return new String[] {"a", "b"};
+    }
 }

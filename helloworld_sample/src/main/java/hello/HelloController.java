85eab278abe9514a1b1ea0ec5872208b689e9b1e
scott.lan <scott.lan@taisys.com>
2014-07-02 17:44:04 +0800

    add json response

--- helloworld_sample/src/main/java/hello/HelloController.java
+++ helloworld_sample/src/main/java/hello/HelloController.java
@@ -14,7 +14,17 @@ public class HelloController {
 
     // application/json 自動轉型態的
     @RequestMapping("/json")
-    public String[] index_json(HttpServletResponse response){
+    public String[] index_json(){
         return new String[] {"a", "b"};
+    }
+
+    // content-type set by self
+    @RequestMapping("/json2")
+    public ResponseEntity<String> index_json(HttpServletResponse response){
+        HttpHeaders headers = new HttpHeaders();
+        // headers.setContentType(MediaType.TEXT_PLAIN);
+        headers.set("content-type", "application/json; charset=UTF-8");
+        return new ResponseEntity<String>("{\"value\": 1}", headers,
+                HttpStatus.OK);
     }
 }
